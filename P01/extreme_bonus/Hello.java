89f3ca5dd1153ff90327e4961ef8c884698513f8
diegoalmonds <mmdiego06@gmail.com>
2023-08-26 18:12:54 -0500

    P01 extreme bonus

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,6 +1,6 @@
 public class Hello
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         String name = System.getProperty("user.home");
         System.out.println("hello, " + name.substring(name.lastIndexOf("/")+1));
