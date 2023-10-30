a6dd8e035db17498193ada533f36851bb1c3fec6
diegoalmonds <mmdiego06@gmail.com>
2023-10-30 18:17:27 -0500

    P08 full credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::string input1;
+    std::cout << "What's your name?" << std::endl;
+    std::cin >> input1;
+    std::cout << "Hello " << input1 << std::endl;
+}
